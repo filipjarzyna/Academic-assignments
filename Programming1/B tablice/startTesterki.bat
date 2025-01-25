cf8bba3c42cc2c916337fcd0a5c2d4925daec6e5
filip <154277389+filipjarzyna@users.noreply.github.com>
2025-01-25 16:45:55 +0100

    Delete Programming1 directory

--- Programming1/B tablice/startTesterki.bat
+++
@@ -1,3 +0,0 @@
-zadanie.exe < .\doEdycji\testy.txt > .\doEdycji\wynikiTwojegoProgramu.txt
-fc /a /w .\doEdycji\janosikOdp.txt .\doEdycji\wynikiTwojegoProgramu.txt > .\doEdycji\wynikiTestu.txt
-start notepad ".\doEdycji\wynikiTestu.txt"
--- Programming1/E towar/startTesterki.bat
+++
@@ -1,3 +0,0 @@
-source2.exe < .\doEdycji\testy.txt > .\doEdycji\wynikiTwojegoProgramu.txt
-fc /a /w .\doEdycji\janosikOdp.txt .\doEdycji\wynikiTwojegoProgramu.txt > .\doEdycji\wynikiTestu.txt
-start notepad ".\doEdycji\wynikiTestu.txt"
